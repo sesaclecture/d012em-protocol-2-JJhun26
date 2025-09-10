c81ca1da41dcddb7c42a1facd4f9a4af503d8c72
JJhun <jungjihun926@gmail.com>
2025-09-10 18:33:34 +0900

    HW

--- src/main.py
+++ src/main.py
@@ -167,7 +167,8 @@ def read_imu() -> Dict[str, int]:
 
     with SMBus(1) as bus:
         # TODO: I2C로 MPU6050에서 6축 값 읽기
-        pass
+        ax, ay, az = (_read_word(bus,Mpu6050Reg.ADDR,Mpu6050Reg.ACCEL_XOUT_H + i ) for i in range(0,5,2))
+        gx, gy, gz = (_read_word(bus,Mpu6050Reg.ADDR,Mpu6050Reg.GYRO_XOUT_H + i ) for i in range(0,5,2))
 
     return {"ax": ax, "ay": ay, "az": az, "gx": gx, "gy": gy, "gz": gz}
 
@@ -180,7 +181,9 @@ def wake_device() -> Tuple[int, int]:
     with SMBus(1) as bus:
         # TODO: PWR_MGMT_1 레지스터 읽고, sleep bit 토글
         before = bus.read_byte_data(Mpu6050Reg.ADDR, Mpu6050Reg.PWR_MGMT_1)
-        verify = "not implemented"
+        after = before ^ (1 << 6)
+        bus.write_byte_data(Mpu6050Reg.ADDR, Mpu6050Reg.PWR_MGMT_1, after)
+        verify = bus.read_byte_data(Mpu6050Reg.ADDR, Mpu6050Reg.PWR_MGMT_1)
 
     return before, verify
 
@@ -194,7 +197,13 @@ def rfid_poll_once() -> Tuple[bool, Optional[bytes]]:
     r = Rc522SPI()
     try:
         # TODO: REQA 전송 후 ATQA 수신
-        return False, None
+        r.antenna_on(True)
+        atqa_bytes = r.transceive_7bit(0x26)
+        if len(atqa_bytes) == 2:
+            present = True
+        else:
+            present = False
+        return present, atqa_bytes
     finally:
         r.close()
 
@@ -208,7 +217,8 @@ def rfid_set_antenna(on: bool) -> int:
     r = Rc522SPI()
     try:
         # TODO: 안테나 on/off 설정
-        return 0
+        r.antenna_on(on)
+        return r.read_reg(Rc522Reg.TX_CONTROL)
     finally:
         r.close()
 
@@ -223,8 +233,8 @@ def ssh_get_arch() -> str:
     archs = ("aarch64", "arm64")
 
     # TODO: user_host, cmd 채우기
-    user_host = ""
-    cmd = ""
+    user_host = "jihun@192.168.100.68"
+    cmd = "uname -m"
 
     if not user_host or not user_host.strip():
         raise ValueError("user_host를 반드시 채우세요.")
